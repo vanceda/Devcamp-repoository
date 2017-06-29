fa54e96107b918e26911de79ae0c435245f976e5
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 15:49:45 +0000

    Added portfloio items via Resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621142403) do
+ActiveRecord::Schema.define(version: 20170628141918) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.text     "main_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
